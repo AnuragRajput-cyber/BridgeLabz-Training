d4d5d55150ec4a2877be4aefd6e762cbc0650280
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-18 00:03:41 +0530

    [Anurag] added solution of question kilometers to miles in gcr-codebase

---
+++ core-java-practice/gcr-codebase/KilometerToIMiles.java
@@ -0,0 +1,10 @@
+package gcr_codebase;
+import java.util.Scanner;
+public class KilometerToIMiles {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		float km=sc.nextFloat();	
+		System.out.println("miles: "+ km*0.621371);
+		
+		}
+}
