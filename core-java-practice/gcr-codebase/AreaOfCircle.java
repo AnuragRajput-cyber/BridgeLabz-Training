16834efb5166ebfcdc1cb3d12e6d03a160a83f08
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-17 15:50:19 +0530

    [Anurag] added 4 pratice questions in gcr-codebase

---
+++ core-java-practice/gcr-codebase/AreaOfCircle.java
@@ -0,0 +1,11 @@
+package gcr_codebase;
+import java.util.Scanner;
+import java.math.*;
+
+public class AreaOfCircle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		System.out.print("Area: "+Math.PI*r*r);
+	}
+}
