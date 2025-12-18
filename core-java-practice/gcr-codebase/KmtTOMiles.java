8b0c15342eed632e058d519e5c15c54594aa729d
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-18 10:58:17 +0530

    [Anurag] added solution of question converting kilometers to miles in gcr-codebase

---
+++ core-java-practice/gcr-codebase/KmtTOMiles.java
@@ -0,0 +1,8 @@
+package gcr_codebase;
+
+public class KmtTOMiles {
+	public static void main(String[] args) {
+		double dis=10.8;
+		System.out.println("The distance of "+dis+" km in miles is "+dis*1.6);
+	}
+}
