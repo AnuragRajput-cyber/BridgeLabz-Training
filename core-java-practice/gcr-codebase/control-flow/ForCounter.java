c30ecfe089ea1e752f93ae69d7ff78c870005f2f
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:34:57 +0530

    [Anurag] added solution of finding counting using for in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/ForCounter.java
@@ -0,0 +1,16 @@
+package gcr_codebase.control_flow;
+
+import java.util.Scanner;
+
+public class ForCounter {
+	static void  CountFor(int num) {
+		for (int i = num; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+    	int num=sc.nextInt();
+        CountFor(num);
+    }
+}
