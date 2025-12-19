cb85d8b5fe57946cced9cfc40de1a8af4d07b6cf
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:38:45 +0530

    [Anurag] added solution of finding factorial of number using while in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/FactorialOfIntegerUsingWhile.java
@@ -0,0 +1,24 @@
+package gcr_codebase.control_flow;
+
+import java.util.Scanner;
+
+public class FactorialOfIntegerUsingWhile {
+	static void factorial(int n) {
+        if (n >= 0) {
+            int fact = 1;
+            int i=1;
+            while(i<=n) {
+            	fact*=i;
+            	i++;
+            }
+            System.out.println(fact);
+        } else {
+            System.out.println("Not a positive integer");
+        }
+    }
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+    	int num=sc.nextInt();
+    	factorial(num);
+    }
+}
