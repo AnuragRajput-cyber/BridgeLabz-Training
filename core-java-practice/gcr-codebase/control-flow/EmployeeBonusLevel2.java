3ca75f727165cf9cb87907f6043da1abfc76ddd9
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:44:10 +0530

    [Anurag] added Lvl2 solution of Employee Bonus in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/EmployeeBonusLevel2.java
@@ -0,0 +1,19 @@
+package gcr_codebase.control_flow;
+
+
+import java.util.Scanner;
+public class EmployeeBonusLevel2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double salary = sc.nextDouble();
+
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = 0.05 * salary;
+            System.out.println(bonus);
+        } else {
+            System.out.println("No bonus for less than or equal to 5 years.");
+        }
+    }
+}
