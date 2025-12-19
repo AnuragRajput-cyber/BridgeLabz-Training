3b0ff4d80f95766ac2b7e310dbac039c28eccf5e
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:35:24 +0530

    [Anurag] added solution of finding Sum until zero in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/SumUntilZero.java
@@ -0,0 +1,17 @@
+package gcr_codebase.control_flow;
+import java.util.Scanner;
+public class SumUntilZero {
+	static void countSum(Scanner sc) {
+        int total = 0, n;
+        while (true) {
+            n = sc.nextInt();
+            if (n == 0) break;
+            total += n;
+        }
+        System.out.println(total);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        countSum(sc);
+    }
+}
