c2f528f88d6dbcfec95f95ef7665682233c4151d
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:43:39 +0530

    [Anurag] added Lvl2 solution of Greatest Factor Using For in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/FactorsWithForLvl2.java
@@ -0,0 +1,14 @@
+package gcr_codebase.control_flow;
+import java.util.Scanner;
+
+public class FactorsWithForLvl2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0)
+                System.out.println(i);
+        }
+    }
+}
