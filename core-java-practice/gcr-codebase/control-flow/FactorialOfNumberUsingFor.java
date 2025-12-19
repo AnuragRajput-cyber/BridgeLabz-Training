faedcda1a48d3395322025b8b25811ded4a23537
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:37:53 +0530

    [Anurag] added solution of finding factorial of number using for in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/FactorialOfNumberUsingFor.java
@@ -0,0 +1,21 @@
+package gcr_codebase.control_flow;
+
+import java.util.Scanner;
+
+public class FactorialOfNumberUsingFor {
+	static void Fact(int n) {
+        if (n >= 0) {
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            System.out.println(fact);
+        } else {
+            System.out.println("Not a positive integer");
+        }
+    }
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+    	int num=sc.nextInt();
+    }
+}
