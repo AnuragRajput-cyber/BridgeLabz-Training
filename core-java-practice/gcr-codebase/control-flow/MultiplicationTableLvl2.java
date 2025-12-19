da18d68569dc05c79d42104241bc4a2c21fab722
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-19 22:42:50 +0530

    [Anurag] added Lvl2 solution of multiplication table in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplicationTableLvl2.java
@@ -0,0 +1,14 @@
+package gcr_codebase.control_flow;
+
+import java.util.Scanner;
+
+public class MultiplicationTableLvl2 {
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
