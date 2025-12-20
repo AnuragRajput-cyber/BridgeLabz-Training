78f97d255e71d4b01e4864cf85c708058d082d07
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-20 23:06:37 +0530

    [Anurag] Refactored: Solution of Odd Even Array gcr-codebase/arrays

--- core-java-practice/gcr-codebase/arrays/OddEvenArray.java
+++ core-java-practice/gcr-codebase/arrays/OddEvenArray.java
@@ -1,42 +1,47 @@
 package gcr_codebase.arrays;
 
 
-import java.util.Scanner;
+import java.util.*;
 
-public class OddEvenArray {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-        
-        if (number <= 0) {
-            System.out.println("Error: Enter a positive number.");
-            return;
+public class OddEven {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in) ;
+
+        int number  = sc.nextInt() ;
+
+        if(number <= 0 ){
+            System.out.println("ERROR");
+            return ;
         }
+        int length = number / 2;
 
-        int[] odd = new int[number / 2 + 1];
-        int[] even = new int[number / 2 + 1];
-        int oddIdx = 0, evenIdx = 0;
+        int[] odd  = new int[number % 2 == 0 ? length : length  + 1] ;
+        int[] even = new int[length];
+        int indexOdd =  0 ;
+        int indexEven  = 0 ;
+        for(int i = 1;  i <= number ; i++){
+            if( i  % 2 != 0 ){
 
-        for (int i = 1; i <= number; i++) {
-            if (i % 2 == 0) {
-                even[evenIdx++] = i;
-            } else {
-                odd[oddIdx++] = i;
+                odd[indexOdd] = i ;
+                indexOdd++;
+
+            }
+            else{
+                even[indexEven] = i ;
+                indexEven++;
             }
         }
-
-        System.out.print("Odd numbers: ");
-        for (int i = 0; i < oddIdx; i++) {
-            System.out.print(odd[i] + " ");
+        System.out.println("ODD NUMBERS ARE  : ") ;
+        for(int v : odd) {
+            System.out.print(v + " " ) ;
         }
-        System.out.println();
-
-        System.out.print("Even numbers: ");
-        for (int i = 0; i < evenIdx; i++) {
-            System.out.print(even[i] + " ");
+        System.out.println() ;
+        System.out.println("EVEN NUMBERS ARE : ") ;
+        for(int v : even ){
+            System.out.print(v + " ") ;
         }
-        System.out.println();
+        System.out.println() ;
     }
-}
 
+}
