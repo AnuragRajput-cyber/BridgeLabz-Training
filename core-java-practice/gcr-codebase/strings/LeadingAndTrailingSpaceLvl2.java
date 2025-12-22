121e2e4eca3f0170712d9fd220d902ac722e69dd
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-22 15:06:40 +0530

    [Anurag] Added: lvl2 Solution of Finding Leading and trailing spaces gcr-codebase/strings

---
+++ core-java-practice/gcr-codebase/strings/LeadingAndTrailingSpaceLvl2.java
@@ -0,0 +1,27 @@
+package gcr_codebase.strings;
+
+import java.util.*;
+
+public class LeadingAndTrailingSpaceLvl2 {
+    public static boolean isVowel(char c) {
+        c = (char)(c >= 'A' && c <= 'Z' ? c + 32 : c);
+        return "aeiou".indexOf(c) >= 0;
+    }
+    public static int[] coutingVowelAndConsonent(String str) {
+        int v = 0, c = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (isVowel(ch)) v++;
+                else c++;
+            }
+        }
+        return new int[]{v, c};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] res = coutingVowelAndConsonent(str);
+        System.out.println("Vowel " + res[0] + ", Consonant " + res[1]);
+    }
+}
