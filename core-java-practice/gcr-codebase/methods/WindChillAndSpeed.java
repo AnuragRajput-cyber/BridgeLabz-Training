86632c74c7bd9e7effca55be42947e270dfc7b7c
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-22 19:15:13 +0530

    [Anurag] Added:Level 1 Solution of Finding chill and speed of wind in gcr_codebase/methods

---
+++ core-java-practice/gcr-codebase/methods/WindChillAndSpeed.java
@@ -0,0 +1,19 @@
+
+package gcr_codebase.methods;
+
+import java.util.Scanner;
+
+public class WindChillAndSpeed {
+    public static double calculateWindChill(double temp,double speed) {
+        return (35.74+0.6215 * temp+(0.4275 * temp - 35.75)* Math.pow(speed, 0.16));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        double temp =sc.nextDouble();
+        double wind= sc.nextDouble();
+
+        double res=calculateWindChill(temp,wind);
+        System.out.println("Wind Chill: " +res);
+    }
+}
