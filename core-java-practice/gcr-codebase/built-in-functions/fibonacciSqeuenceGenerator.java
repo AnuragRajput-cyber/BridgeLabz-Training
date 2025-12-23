ae8f6e01fceae738bd00cd1456b0544ccf2ec7e6
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-23 11:50:22 +0530

    [Anurag] Added: Solution Of generating fibonacci sequence in gcr-codebase/built-in-functions

---
+++ core-java-practice/gcr-codebase/built-in-functions/fibonacciSqeuenceGenerator.java
@@ -0,0 +1,29 @@
+package gcr_codebase.built_In_function;
+
+import java.util.Scanner;
+
+public class fibonacciSqeuenceGenerator{
+    public static void generateFibonacci(int n){
+        int first=0;
+        int second=1;
+        for(int i=0;i<n;i++){
+            System.out.print(first+" ");
+            int next=first+second;
+            first=second;
+            second=next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n<=0){
+            System.out.println("Enter positive number");
+        }else{
+            generateFibonacci(n);
+        }
+    }
+}
+
