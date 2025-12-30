2fcb8408b74d49b7be16cfa529bbb55da909cde4
AnuragRajput-cyber <andrenotech123@gmail.com>
2025-12-30 10:23:49 +0530

    [Anurag] added:Solution of practice Problems of stack,queue, hashMap and HashFunctions in gcr-codebase/stack-queue-hashMap-and-hashFunctions

---
+++ dsa-practice/gcr-codebase/stack-queue-hashmaps-and-hash-funtions/hashMap_and_hashFuntions/TwoSum.java
@@ -0,0 +1,27 @@
+package dsa_practice.stack_queue_hashmaps_and_hash_funtions.hashMap_and_hashFuntions;
+
+import java.util.*;
+
+public class TwoSum {
+    public static int[] findTwoSum(int[]nums,int target){
+        Map<Integer,Integer> numMap=new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if (numMap.containsKey(complement)){
+                return new int[]{numMap.get(complement),i};
+            }
+            numMap.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args){
+        int[]nums={2,7,11,15};
+        int target=9;
+        int[]result=findTwoSum(nums,target);
+        if(result.length==2){
+            System.out.println("Indices: "+result[0] +", "+result[1]);
+        }else{
+            System.out.println("No pair found.");
+        }
+    }
+}
