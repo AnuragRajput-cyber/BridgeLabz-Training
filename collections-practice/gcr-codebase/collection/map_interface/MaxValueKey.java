bc17f734811375315c1fa4584d37348d51501ca8
AnuragRajput-cyber <andrenotech123@gmail.com>
2026-01-05 23:09:13 +0530

    [Anurag] added:Solution of Collection Practice Problems of Map Interface in collection-practice/collection/map-interface

---
+++ collections-practice/gcr-codebase/collection/map_interface/MaxValueKey.java
@@ -0,0 +1,27 @@
+package collections_practice.collection.map_interface;
+
+import java.util.*;
+public class MaxValueKey {
+  public static void main(String[] args) {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("A", 10);
+    map.put("B", 20);
+    map.put("C", 15);
+    String maxKey = findKeyWithMaxValue(map);
+    System.out.println(maxKey);
+  }
+
+
+  public static String findKeyWithMaxValue(Map<String, Integer> map){
+    String maxKey = null;
+    int maxValue = Integer.MIN_VALUE;
+ 
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      if (entry.getValue() > maxValue) {
+        maxValue = entry.getValue();
+        maxKey = entry.getKey();
+      }
+    }
+    return maxKey;
+  }
+}
