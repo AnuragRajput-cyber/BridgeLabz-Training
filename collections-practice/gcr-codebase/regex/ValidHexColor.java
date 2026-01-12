49c945fce7cf50545f863ce0d2eee5bb629479f4
AnuragRajput-cyber <andrenotech123@gmail.com>
2026-01-12 16:51:08 +0530

    [Anurag] added:Solution of Practice Problems on Regex in collection-practice/gcr-codebase/regex

---
+++ collections-practice/gcr-codebase/regex/ValidHexColor.java
@@ -0,0 +1,20 @@
+package collections_practice.regex;
+
+import java.util.regex.*;
+
+public class ValidHexColor {
+	public static void main(String[] args) {
+		String color="#Fd5490";
+		String regex="^#[0-9a-fA-F]{6}";
+		
+		Pattern pattern=Pattern.compile(regex);
+		
+		Matcher matcher=pattern.matcher(color);
+		
+		if(matcher.matches()) {
+			System.out.println("Valid Color");
+		}else {
+			System.out.println("Invalid Color");
+		}
+	}
+}
