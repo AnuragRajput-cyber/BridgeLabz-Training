c628e675f408984b63f664b3c814c1842e448bf8
AnuragRajput-cyber <andrenotech123@gmail.com>
2026-01-30 16:08:33 +0530

    [Anurag] added : Solution of scenario-based problem JUNIT-TESTCASE in collection-practice/scenario-based/Junit-Test case

---
+++ collections-practice/scenario-based/Junit-TestCase/UnitTest.java
@@ -0,0 +1,24 @@
+package collections_practice.scenario_based;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
+public class UnitTest {
+	private Program program;
+	
+	@BeforeEach
+	public void setup() {
+		program=new Program(15000);
+	}
+	
+	@Test
+	 void test_Deposite_ValidAmount() throws NegativeAmountException {
+		program.deposite(5000);
+		assertEquals(20000,program.getBalance());
+	}
+	
+	@Test
+	void test_Withdraw_ValidAmount() throws InsufficientBalanceException {
+		program.withdraw(5000);
+		assertEquals(10000,program.getBalance());
+	}
+}
