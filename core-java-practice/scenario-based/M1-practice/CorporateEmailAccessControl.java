39f4b26c25f996f61fa0702378aaaff50153b9b5
AnuragRajput-cyber <andrenotech123@gmail.com>
2026-03-17 11:05:37 +0530

    [Anurag] added:Solution of M1 practice Questions.

---
+++ core-java-practice/scenario-based/M1-practice/CorporateEmailAccessControl.java
@@ -0,0 +1,14 @@
+package m1.practice;
+
+import java.util.Scanner;
+
+public class CorporateEmailAccessControl {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		sc.nextLine();
+		while(n-->0) {
+			
+		}
+	}
+}
